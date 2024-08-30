29e8a7ae30043032e1b008061e63e39a2da30b0d
ThuroGamarques <141521374+ThuroGamarques@users.noreply.github.com>
2024-08-30 23:18:32 +0000

    Oia

--- app/componentes/adaptadores/lista-produtos/index.tsx
+++ app/componentes/adaptadores/lista-produtos/index.tsx
@@ -2,24 +2,23 @@ import { ScrollView, View } from "react-native";
 import ItemProduto from "../item-produto";
 import style from "@/app/style/default";
 
-// Define o tipo para o produto
 type Produto = {
-  id: number;
-  nome: string;
-  preco: number;
+    id: number;
+    nome: string;
+    preco: number;
 };
 
-// Corrige o tipo de produtos para ser um array de objetos Produto
-function ListaProdutos({ produtos }: { produtos: Produto[] }) {
-  return (
-    <ScrollView>
-      <View>
-        {produtos.map((p) => (
-          <ItemProduto produto={p} key={p.id} />
-        ))}
-      </View>
-    </ScrollView>
-  );
+function ListaProdutos({ produtos }: { produtos: Produto[] })
+{
+    return (
+        <ScrollView>
+            <View>
+                {produtos.map((p) => (
+                    <ItemProduto produto={p} key={p.id} />
+                ))}
+            </View>
+        </ScrollView>
+    );
 }
 
 export default ListaProdutos;
--- app/index.tsx
+++ app/index.tsx
@@ -1,60 +1,70 @@
 import { useState } from "react";
 import { Text, View, StyleSheet, Button } from "react-native";
+import axios from 'axios'
+import { useEffect} from 'react'
 
 export default function Index()
 {
-  let [contador, setContador] = useState(0);
-  let [produtos, setProdutos] = useState(1);
+	let [contador, setContador] = useState(0);
+	let [produtos, setProdutos] = useState(1);
 
-  // const produtos = [
-    // {id: 1, nome: "Coca-cola", preco: 12.5},
-    // {id: 2, nome: "Pepsi", preco: 8.88},
-    // {id: 3, nome: "Fanta", preco: 8.95},
-    // {id: 4, nome: "Guaraná", preco: 6.5},
-  // ];
+	useEffect(() => 
+	{
+		carregarProdutos();
+	}, []);
 
-  function carregarProdutos()
-  {
-    
-  }
+	// const produtos = [
+	// {id: 1, nome: "Coca-cola", preco: 12.5},
+	// {id: 2, nome: "Pepsi", preco: 8.88},
+	// {id: 3, nome: "Fanta", preco: 8.95},
+	// {id: 4, nome: "Guaraná", preco: 6.5},
+	// ];
 
-  return (
-    <View style={main.view}>
-      {produtos.map((p)=> (
-        <View key={p.id}>
-        <Text style =  {main.titulo}>{p.nome} </Text>
-        <Text>{p.preco}</Text>
-        </View>
-      ))}
-      <Button title= {contador.toString()} onPress={()=>{clicarBotao()}}></Button>
-    </View>
-  );
+	function carregarProdutos()
+	{
+		axios.get('https://app-api-tapwm.onrender.com/api/produtos').then((resp) =>
+		{
 
-  function clicarBotao(){
-    setContador(contador+1);
-  }
+		});
+  	}
+
+	return (
+		<View style={main.view}>
+			{produtos.map((p) => (
+				<View key={p.id}>
+					<Text style={main.titulo}>{p.nome} </Text>
+					<Text>{p.preco}</Text>
+				</View>
+			))}
+			<Button title={contador.toString()} onPress={() => { clicarBotao() }}></Button>
+		</View>
+	);
+
+	function clicarBotao()
+	{
+		setContador(contador + 1);
+	}
 }
 
-
-const main=StyleSheet.create(
+const main = StyleSheet.create(
 {
-  view:
-  {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#482973"
-  },
-  text:
-  {
-    color: "#eb34d8",
-    fontSize: 50,
-    fontWeight: "bold"
-  },
-  titulo:{
-    color:'#FFFFFF',
-    fontSize: 30,
-    textAlign: "left",
-    fontWeight: "bold"  
-  }
+	view:
+	{
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+		backgroundColor: "#482973"
+	},
+	text:
+	{
+		color: "#eb34d8",
+		fontSize: 50,
+		fontWeight: "bold"
+	},
+	titulo: {
+		color: '#FFFFFF',
+		fontSize: 30,
+		textAlign: "left",
+		fontWeight: "bold"
+	}
 });
